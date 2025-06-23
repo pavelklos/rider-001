c3aaebaa3e4c8c8842f7ee656beab796fe2516d0
Pavel Klos <pavelkloscz@gmail.com>
2025-06-23 09:56:13 +0200

    Live Templates

--- DometrainRiderPlayground/Implementations/MyImplementation.cs
+++ DometrainRiderPlayground/Implementations/MyImplementation.cs
@@ -15,10 +15,29 @@ public class MyImplementation : IMyInterface
         var items = MyPrivateMethod();
 
         var foo = Foo(items);
+        
+        // Postfix Completion
+        // https://www.jetbrains.com/help/rider/Settings_Postfix_Completion.html
+        // foo.notnull => if (foo != null) {}
+        // items.foreach => foreach (var item in items) {}
 
         Console.WriteLine(foo);
-
-        foreach (var item in items) Console.WriteLine(item);
+        
+        // foreach (var item in items) Console.WriteLine(item);
+        
+        // Live Templates (foreach) + Surround with Live Template (try)
+        foreach (var item in items)
+        {
+            try
+            {
+                Console.WriteLine(item);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
     }
 
     // private void MyPrivateMethod()
@@ -61,3 +80,14 @@ public class MyImplementation : IMyInterface
         return ["Foo", "Bar"];
     }
 }
+
+// Live Templates (New Template) "mhandler" for "MediatR Handler"
+// https://github.com/jbogard/MediatR
+// https://github.com/jbogard/MediatR/wiki#requestresponse
+// public class GetProductsHandler : IRequestHandler<GetProducts, string>
+// {
+//     public Task<string> Handle(GetProducts request, CancellationToken cancellationToken)
+//     {
+//         
+//     }
+// }
