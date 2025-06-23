51cf247195227bf424b574fe31157aa41ff97c9c
Pavel Klos <pavelkloscz@gmail.com>
2025-06-23 11:21:00 +0200

    DometrainDebuggingPlayground.Tests

---
+++ DometrainDebuggingPlayground.Tests/PodcastTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System.Net;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace DometrainDebuggingPlayground.Tests;
+
+public class PodcastTests
+{
+    [Fact]
+    public async Task GetPodcastsTests()
+    {
+        var client = new WebApplicationFactory<Program>().CreateClient();
+        var response = await client.GetAsync("/podcasts");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+}
