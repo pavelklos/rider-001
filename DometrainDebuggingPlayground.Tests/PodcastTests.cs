54d41f3ef89d42467d2476e7925853232102c634
Pavel Klos <pavelkloscz@gmail.com>
2025-06-25 19:54:15 +0200

    Add new tests

--- DometrainDebuggingPlayground.Tests/PodcastTests.cs
+++ DometrainDebuggingPlayground.Tests/PodcastTests.cs
@@ -14,4 +14,22 @@ public class PodcastTests
         var response = await client.GetAsync("/podcasts");
         response.StatusCode.Should().Be(HttpStatusCode.OK);
     }
-}
+    
+    // Test to check if podcasts endpoint returns 200 OK status code for valid genre
+    [Fact]
+    public async Task GetPodcastsBySearchTerm_ValidSearchTerm_ReturnsOk()
+    {
+        var client = new WebApplicationFactory<Program>().CreateClient();
+        var response = await client.GetAsync("/podcasts/bygenre?genre=dotnet");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+    
+    // Test to check if podcasts endpoint returns 404 Not Found status code for invalid genre
+    [Fact]
+    public async Task GetPodcastsByGenre_InvalidGenre_ReturnsNotFound()
+    {
+        var client = new WebApplicationFactory<Program>().CreateClient();
+        var response = await client.GetAsync("/podcasts/bygenre?genre=invalid");
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+}       
