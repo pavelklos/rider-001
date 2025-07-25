463c03ae76ade4d7d0e685ea3ac3387bcd6d0434
Pavel Klos <pavelkloscz@gmail.com>
2025-07-25 20:22:41 +0200

    ShoppingCartLib.Tests - UPDATE

--- ShoppingCartLib.Tests/UnitTest1.cs
+++ ShoppingCartLib.Tests/UnitTest1.cs
@@ -40,15 +40,70 @@ public class UnitTest1(ITestOutputHelper testOutputHelper)
     }
 
     [Fact]
+    public void RemoveProduct()
+    {
+        var sut = new ShoppingCart();
+        sut.Add("Docker course");
+        sut.Add("Kubernetes course");
+        sut.Remove("Docker course");
+        sut.Get()
+            .Should()
+            .BeEquivalentTo(["Kubernetes course"]);
+    }
+
+    [Fact]
+    public void ContainsProduct()
+    {
+        var sut = new ShoppingCart();
+        sut.Add("Docker course");
+        sut.Contains("Docker course")
+            .Should()
+            .BeTrue();
+    }
+
+    [Fact]
+    public void DoesNotContainProduct()
+    {
+        var sut = new ShoppingCart();
+        sut.Add("Docker course");
+        sut.Contains("Kubernetes course")
+            .Should()
+            .BeFalse();
+    }
+
+    [Fact]
+    public void ClearCart()
+    {
+        var sut = new ShoppingCart();
+        sut.Add("Docker course");
+        sut.Add("Kubernetes course");
+        sut.Clear();
+        sut.Get()
+            .Should()
+            .BeEquivalentTo([]);
+    }
+
+    [Fact]
+    public void CountProducts()
+    {
+        var sut = new ShoppingCart();
+        sut.Add("Docker course");
+        sut.Add("Kubernetes course");
+        sut.Count()
+            .Should()
+            .Be(2);
+    }
+
+    [Fact]
     public void TestWithResultTrue()
     {
         Assert.True(true);
     }
 
-    [Fact]
-    public void TestWithResultFalse()
-    {
-        // This test is intentionally failing
-        Assert.True(false);
-    }
+    // [Fact]
+    // public void TestWithResultFalse()
+    // {
+    //     // This test is intentionally failing
+    //     Assert.True(false);
+    // }
 }
