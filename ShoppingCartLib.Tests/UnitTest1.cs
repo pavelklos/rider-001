25482ea6c88c50899949e1595986301a11067a4a
Pavel Klos <pavelkloscz@gmail.com>
2025-07-25 10:10:04 +0200

    ShoppingCartLib.Tests

---
+++ ShoppingCartLib.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+
+namespace ShoppingCartLib.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void ANewCartShouldBeEmpty()
+    {
+        var sut = new ShoppingCart();
+        sut.Get().Should().BeEquivalentTo([]);
+    }
+
+    [Fact]
+    public void AddAProduct()
+    {
+        var sut = new ShoppingCart();
+        sut.Add("Docker course");
+        sut.Get().Should().BeEquivalentTo(["Docker course"]);
+    }
+}
