75bce48ddf474b34a57672e82a080bfea1f8d40b
Pavel Klos <pavelkloscz@gmail.com>
2025-08-07 06:13:46 +0200

    PerformanceModule.Tests

---
+++ PerformanceModule.Tests/FibonacciCalculatorTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+
+namespace PerformanceModule.Tests;
+
+public class FibonacciCalculatorTests
+{
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(2, 1)]
+    [InlineData(3, 2)]
+    [InlineData(7, 13)]
+    [InlineData(8, 21)]
+    public void Test(int count, int expected)
+    {
+        var fibCalculator = new FibonacciCalculator();
+        var result = fibCalculator.Calculate(count);
+        result.Should().Be(expected);
+    }
+}
