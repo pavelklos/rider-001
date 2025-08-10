91184b66b8ccc11b489fd344b0e91b41887c1bd0
Pavel Klos <pavelkloscz@gmail.com>
2025-08-10 23:08:37 +0200

    FibonacciCalculator.cs

---
+++ FibonacciCalculator.cs
@@ -0,0 +1,21 @@
+﻿namespace PerformanceModule;
+
+public class FibonacciCalculator
+{
+    public int Calculate(int n)
+    {
+        if (n == 0) return 0;
+
+        var prev = new[] { 0, 1 };
+        var current = 1;
+
+        for (var i = 2; i <= n; i++)
+        {
+            current = prev[0] + prev[1];
+            prev[0] = prev[1];
+            prev[1] = current;
+        }
+
+        return current;
+    }
+}
